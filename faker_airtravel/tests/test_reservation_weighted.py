70e5fb0a49a35da64850712f8e1fef861b0fe2ed
Emanuele Concas <conema@outlook.com>
2023-06-21 11:42:18 +0200

    Test reservation weighted

---
+++ faker_airtravel/tests/test_reservation_weighted.py
@@ -0,0 +1,64 @@
+from faker import Faker
+
+from faker_airtravel import ReservationProvider
+from faker_airtravel.constants import cabin_class as cc
+
+fake = Faker()
+fake.add_provider(ReservationProvider)
+
+def test_number_pax():
+    number_pax = fake.number_pax(
+        min=2,
+        max=5,
+        weights=[1, 0, 0, 0, 0]
+    )
+
+    assert number_pax == 2
+
+def test_frequent_flyer():
+    frq_flr = fake.frequent_flyer(
+        weights=[1, 0]
+    )
+
+    assert frq_flr == 0
+
+def test_leg_number():
+    leg_number = fake.leg_number(
+        min=1,
+        max=3,
+        weights=[0.5, 0.5, 0]
+    )
+
+    assert leg_number in [1, 2, 3]
+
+def test_cabin_class():
+    cabin_class = fake.cabin_class(
+        weights = [0.5, 0.5, 0, 0]
+    )
+
+    assert cabin_class in cc[:2]
+
+def test_passenger():
+    def price_function(**args):
+        return 5
+    
+    weights = {
+        "number_pax": [1, 0, 0, 0, 0],
+        "frq_flr": [1, 0],
+        "leg_num": [0.5, 0.5, 0],
+        "cabin_class": [0.5, 0.5, 0, 0]
+    }
+
+    passenger = fake.passenger(
+        min_max_pax=(2,5),
+        min_max_leg=(1,3),
+        start_end_res=("now", "now"),
+        price_function=price_function,
+        weights=weights
+    )
+
+    assert passenger.get("ticket_price") == 5
+    assert passenger.get("number_pax") == 2
+    assert passenger.get("frq_flr") == 0
+    assert passenger.get("leg_number") in [1, 2, 3]
+    assert passenger.get("cabin_class") in cc[:2]
